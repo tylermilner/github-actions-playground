77ba7dfab8520abfdefc245230a3cebe9fae3152
Tyler Milner <tylermilner@gmail.com>
2023-06-30 22:02:11 -0500

    GHADemo project for basic iOS CI demo.

---
+++ SimpleGHADemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleGHADemo
+//
+//  Created by Tyler Milner on 6/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
