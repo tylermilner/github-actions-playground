25c927886f971e5e687bf137db997d5245ccdbc9
Tyler Milner <tylermilner@gmail.com>
2023-09-04 16:59:05 -0500

    Fix rest of generate-release-notes script.

--- .github/actions/generate-release-notes-action/generate-release-notes.sh
+++ .github/actions/generate-release-notes-action/generate-release-notes.sh
@@ -7,21 +7,21 @@ if [[ -z "$INPUT_END_SHA" ]]; then
 fi
 
 # Generate release notes from commits
-if [[ -z $INPUT_BEGIN_SHA ]]; then
+if [[ -z "$INPUT_BEGIN_SHA" ]]; then
     # Missing beginning commit SHA, default to full commit history
-    echo 'Missing `begin-sha`. Generating release notes for full commit history...'
-    RELEASE_NOTES = $(git log --oneline --no-decorate)
+    echo 'Missing `begin-sha` input. Generating release notes for full commit history...'
+    RELEASE_NOTES=$(git log --oneline --no-decorate)
 else
     # Get the commit history between begin and end SHAs
     echo "Generating release notes between $INPUT_BEGIN_SHA and $INPUT_END_SHA..."
-    RELEASE_NOTES = $(git log --oneline --no-decorate $INPUT_BEGIN_SHA..$INPUT_END_SHA)
+    RELEASE_NOTES=$(git log --oneline --no-decorate $INPUT_BEGIN_SHA..$INPUT_END_SHA)
 fi
 
 # Remove the commit hash at the beginning of each line
-RELEASE_NOTES = $RELEASE_NOTES | cut -d ' ' -f2-
+RELEASE_NOTES=$(echo "$RELEASE_NOTES" | cut -d ' ' -f2-)  
 
 # Add a dash at the beginning of each line
-RELEASE_NOTES = $RELEASE_NOTES | sed 's/^/- /'
+RELEASE_NOTES=$(echo "$RELEASE_NOTES" | sed 's/^/- /')
 
 echo "Generated release notes:"
 echo "$RELEASE_NOTES"
